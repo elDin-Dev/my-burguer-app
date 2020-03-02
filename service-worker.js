6dd7ee5ef70a40bf6a83161ab7d0bf6da8fbdd7a
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-03-02 12:51:26 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-burguer-app/precache-manifest.4dafe42217774989eccae365a7b046e5.js"
+  "/my-burguer-app/precache-manifest.451bfc7ab19ede87331ea6bc89d2108c.js"
 );
 
 self.addEventListener('message', (event) => {

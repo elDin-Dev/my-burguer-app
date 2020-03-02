75ae44564d0abcc25c28c0313b37bb962727aa12
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-03-02 09:48:09 +0100

    dynamic ingredients arrays

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliar";
+import Burger from "../../components/Burger/Burger";
+
+class BurgerBuilder extends Component {
+
+  
+
+  state = {
+    ingredients: {
+      salad:1,
+      bacon:1,
+      cheese:2,
+      meat:2,
+    }
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Contols</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;

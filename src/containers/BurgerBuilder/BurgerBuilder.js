c9ab38c2f283cb1e502760bcb28321bad4596a35
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-03-03 08:32:02 +0100

    showing the modal with animation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchaseable: false
+    purchaseable: false,
+    purchasing: false,
   };
 
   addIngredientHandler = type => {
@@ -65,6 +66,9 @@ class BurgerBuilder extends Component {
     this.setState({ purchaseable: sum > 0 });
   }
 
+  purchaseHandler =() => {
+    this.setState({purchasing:true});
+  }
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -76,7 +80,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal >
+        <Modal show={this.state.purchasing } >
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -85,6 +89,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchaseable={this.state.purchaseable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice}
         />
       </Aux>

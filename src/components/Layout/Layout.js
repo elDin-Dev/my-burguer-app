e19dc475c362226a4810956cd82f904391c6ae3a
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-03-04 23:46:27 +0100

    before apply max code manually

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 class Layout extends Component {
 
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   }
 
   sideDrawerCloseHandler = () => {
@@ -15,10 +15,18 @@ class Layout extends Component {
 
   }
 
+  sideDrawerToggleHandler = () => {
+    this.setState(
+      (prevState) => {
+        return {showSideDrawer: !prevState.showSideDrawer};
+      }
+    );
+  }
+  
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
          open={this.state.showSideDrawer}
          closed={this.sideDrawerCloseHandler} />

fae2e9a3a1f47033607da596ba6157d46f68d416
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-04-22 00:17:10 +0200

    query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,16 +3,28 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 export class Checkout extends Component {
 
-    state={
+    state = {
         ingredients: {
             salad: 1,
-            meat:1,
-            cheese:1,
-            bacon:1
+            meat: 1,
+            cheese: 1,
+            bacon: 1
         }
     }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            ingredients[param[0]] = + param[1];
+        }
+
+        this.setState({ingredients:ingredients});
+    }
+
+
     checkoutCancelledHandler = () => {
-     
+
         this.props.history.goBack();
     }
 
@@ -21,16 +33,16 @@ export class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data');
 
     }
-    
 
-onCheckoutCancelled
+
+    onCheckoutCancelled
     render() {
         return (
             <div>
                 <CheckoutSummary
-                 onCheckoutContinued={this.checkoutContinuedHandler}
-                 onCheckoutCancelled={this.checkoutCancelledHandler}
-                 ingredients={this.state.ingredients}/>
+                    onCheckoutContinued={this.checkoutContinuedHandler}
+                    onCheckoutCancelled={this.checkoutCancelledHandler}
+                    ingredients={this.state.ingredients} />
             </div>
         );
     }

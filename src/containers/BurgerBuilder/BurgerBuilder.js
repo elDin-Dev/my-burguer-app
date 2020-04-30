771b24dc22549a72ea62ad8d95f3d0c8b0f8a7c7
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-04-30 17:49:56 +0200

    update rotal price with reducers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,15 +10,9 @@ import withErrorHandler from '../../hoc/withErroHandler/withErrorHandler';
 import { connect } from 'react-redux';
 import * as actionTypes from '../../store/actions';
 
-const INGREDIENT_PRICES = {
-  salad: 0.5,
-  cheese: 0.4,
-  meat: 1.3,
-  bacon: 0.7
-};
+
 class BurgerBuilder extends Component {
   state = {
-    totalPrice: 4,
     purchaseable: false,
     purchasing: false,
     loading: false,
@@ -42,36 +36,6 @@ class BurgerBuilder extends Component {
       */
   }
 
-  addIngredientHandler = type => {
-    const oldCount = this.state.ingredients[type];
-    const updatedCount = oldCount + 1;
-    const updatedIngredients = {
-      ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCount;
-    const priceAddition = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceAddition;
-    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-    this.updatePurchageState(updatedIngredients);
-  };
-
-  removeIngredientHandler = type => {
-    const oldCount = this.state.ingredients[type];
-
-    if (oldCount <= 0) return;
-
-    const updatedCount = oldCount - 1;
-    const updatedIngredients = {
-      ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCount;
-    const priceDeduction = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction;
-    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-    this.updatePurchageState(updatedIngredients);
-  };
 
   updatePurchageState(ingredients) {
     const sum = Object.keys(ingredients)
@@ -98,7 +62,7 @@ class BurgerBuilder extends Component {
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    queryParams.push('price=' + this.state.totalPrice);
+    queryParams.push('price=' + this.props.price);
 
     const queryString = queryParams.join('&');
     this.props.history.push({
@@ -128,13 +92,13 @@ class BurgerBuilder extends Component {
           disabled={disabledInfo}
           purchaseable={this.state.purchaseable}
           ordered={this.purchaseHandler}
-          price={this.state.totalPrice}
+          price={this.props.price}
         />
       </Aux>
 
       orderSummary = <OrderSummary
         ingredients={this.props.ings}
-        price={this.state.totalPrice}
+        price={this.props.price}
         purchaseCanceled={this.purchaseCancelHandler}
         purchaseContinued={this.purchaseContinueHandler}
       />;
@@ -157,7 +121,8 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    ings: state.ingredients
+    ings: state.ingredients,
+    price: state.totalPrice,
   };
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actions';
 
+
 const initialState = {
 
     ingredients: {
@@ -11,6 +12,12 @@ const initialState = {
     totalPrice: 4,
 }
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+  };
 
 const reducer = (state = initialState, action) => {
 
@@ -22,7 +29,8 @@ const reducer = (state = initialState, action) => {
                  ingredients: {
                      ...state.ingredients,
                      [action.ingredientName]: state.ingredients[action.ingredientName]+1,
-                 }
+                 },
+                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
             };
 
         case actionTypes.REMOVE_INGREDIENT:
@@ -31,7 +39,8 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [action.ingredientName]: state.ingredients[action.ingredientName]-1,
-                }
+                },
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
             };
 
         default:

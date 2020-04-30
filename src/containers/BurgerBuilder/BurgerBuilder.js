20bc3657705bde68e4a14927591a4f4c3df2663f
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-04-30 17:56:07 +0200

    updating purchaable order form redux state to ui state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchaseable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -45,7 +44,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchaseable: sum > 0 });
+    return  sum > 0 ;
   }
 
   purchaseHandler = () => {
@@ -90,7 +89,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.props.onIngredientAdded}
           ingredientRemoved={this.props.onIngredientRemoved}
           disabled={disabledInfo}
-          purchaseable={this.state.purchaseable}
+          purchaseable={this.updatePurchageState(this.props.ings)}
           ordered={this.purchaseHandler}
           price={this.props.price}
         />

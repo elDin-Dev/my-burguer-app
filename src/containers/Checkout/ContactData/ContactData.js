5bac22ce73dc20ba6f9dae81d0d5bf5d63177944
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-05-06 16:11:58 +0200

    adding order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -115,20 +115,6 @@ export class ContactData extends Component {
             orderData: formData,
         }
 
-        axios.post('/orders.json/', order)
-            .then(response => {
-                console.log(response)
-                this.setState({ loading: false });
-                this.props.history.push('/');
-
-            })
-            .catch(error => {
-                console.log(error)
-                this.setState({ loading: false });
-            });
-
-
-
     }
 
     checkValidity(value, rules) {
---
+++ src/store/actions/order.js
@@ -0,0 +1,30 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-order';
+
+export const purchaseBurgerSucess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData,
+    }
+}
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        orderId: error,
+    }
+}
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json/', orderData)
+            .then(response => {
+                console.log(response.data);
+                dispatch(purchaseBurgerSucess(response.data, orderData));
+            })
+            .catch(error => {
+                dispatch(purchaseBurgerFail(error));
+            });        
+    }
+}

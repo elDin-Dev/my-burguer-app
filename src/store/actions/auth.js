d82273c69a3bdc2b2cc712cc27c8766fa0a1a84b
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-05-21 00:10:10 +0200

    getting a token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -23,5 +24,28 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            resturnSecureToken: true,
+
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgTNhKT4929oCXB0-pze0cuEU_m06aoDU',authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        })
+        .catch(err => {
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(err.response.data);
+              } else if (err.request) {
+                console.log(err.request);
+              } else {
+                console.log("Error", err.message);
+              }
+            dispatch(authFail(err));
+        });
     };
 };

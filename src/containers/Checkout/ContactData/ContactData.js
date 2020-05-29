ab1c1260665eead6f46af1499963f7b246e55aa5
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-05-29 12:43:38 +0200

    filter orders by user

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -115,6 +115,7 @@ export class ContactData extends Component {
             ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData,
+            userId: this.props.userId,
         }
 
         this.props.onOrderBurger(order, this.props.token);
@@ -219,7 +220,8 @@ const mapStateToProps = state => {
         ings:state.burgerBuilder.ingredients,
         price:state.burgerBuilder.totalPrice,
         loading:state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId,
     }
 }
 const mapDispatchToProps = dispatch => {

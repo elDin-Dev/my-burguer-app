35e9ba9806b26795cb13f555e315b0accaaa748a
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-06-02 00:14:13 +0200

    adding more test to a suite +  onBefore

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,8 +7,23 @@ import NavigationItem from './NavigattionItem/NavigationItem';
 configure ({adapter: new Adapter()});
 
 describe('<NavigationItem />', () => {
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
     it('should  render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
+
+    it('should  render three <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+
+
 });
+

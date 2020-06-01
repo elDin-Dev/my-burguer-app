e04c711cbdb07dbb968fc0a9e5bb53c61d442d21
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-06-02 00:27:24 +0200

    jest and enzyme

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -14,16 +14,18 @@ describe('<NavigationItem />', () => {
         wrapper = shallow(<NavigationItems />);
     });
 
-    it('should  render two <NavigationItem /> elements if not authenticated', () => {
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
 
-    it('should  render three <NavigationItem /> elements if authenticated', () => {
+    it('should render three <NavigationItem /> elements if authenticated', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
-
-
+    it('should render a <NavigationItem link="/logout" /> element if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
 
